5f27e05eb20dbda8155f631fabf9dec1ed534053
zahratalita <zahratalitapr12@gmail.com>
2022-11-17 00:17:33 +0700

    menambahkan bagian header pada web

---
+++ views/v_header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Prog II | Merancang Template sederahana dengan Codeigniter</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css">
+    
+    <!-- <link rel="stylesheet" type="text/css" href="http://localhost/pustaka-booking/assets/css/stylebuku.css"> -->
+</head>
+<body>
+    <div id="wrapper">
+        <header>
+            <hgroup>
+                <h1>RentalBuku.net</h1>
+                <h3>Membuat Template Sederhana dengan CodeIgniter</h3>
+            </hgroup>
+            <nav>
+
+                <ul>
+                    <li>
+                        <a href="<?php echo base_url().'index.php/web' ?>">
+                        Home
+                        </a>
+                    </li>
+                </ul>
+                
+                <ul>
+                    <li>
+                        <a href="<?php echo base_url().'index.php/about' ?>">
+                        About
+                        </a>
+                    </li>
+                </ul>
+
+            </nav>
+
+            <div class="clear"></div>
+        </header>
+    </div>
